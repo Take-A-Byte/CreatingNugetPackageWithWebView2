ee1f73e5271fcbef7e9b049560cb6158b0af4ea6
Shantanu Methikar <smethikar18@gmail.com>
2023-04-19 17:52:24 +0530

    Add project files.

---
+++ SDK/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.UI.Xaml.Controls;
+
+namespace SDK
+{
+    /// <summary>
+    /// Temp class
+    /// </summary>
+    public class Class1
+    {
+        public Class1()
+        {
+
+        }
+    }
+
+    public class PDFView
+    {
+        private WebView2 _webView2;
+
+        public PDFView()
+        {
+            _webView2 = new WebView2();
+        }
+
+        public void Initialize()
+        {
+
+        }
+    }
+}
---
+++ SDK/CustomControl1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+
+// The Templated Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234235
+
+namespace SDK
+{
+    /// <summary>
+    /// CustomControl1
+    /// </summary>
+    public sealed class CustomControl1 : Control
+    {
+        /// <summary>
+        /// CustomControl1 constructor
+        /// </summary>
+        public CustomControl1()
+        {
+            this.DefaultStyleKey = typeof(CustomControl1);
+        }
+    }
+}
